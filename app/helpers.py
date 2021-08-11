06dcdd6918359c6cdb7b7787473a1ba48ed73cbb
Imon Roy <royimonroy@gmail.com>
2021-08-11 21:49:35 +0530

    bugfix

--- app/helpers.py
+++ app/helpers.py
@@ -3,6 +3,6 @@
 def  objidconv(inp):
     dic={}
     for keys,values in inp.items():
-        if not (keys!='_id' or keys!='uname' or keys!='posts' or keys!='groups' or keys!='last_login'):
+        if (keys=='_id' or keys=='uname' or keys=='posts' or keys=='groups' or keys=='last_login'):
             dic[keys]=values
     return dic

90b56efa45d5a818a24ba6d7f74df81ca7537b3b
Imon Roy <royimonroy@gmail.com>
2021-08-11 22:04:18 +0530

    update

--- app/helpers.py
+++ app/helpers.py
@@ -4,7 +4,9 @@ def  objidconv(inp):
     dic={}
     for keys,values in inp.items():
         if(keys=='_id'):
-            dic[keys]=ObjectId(values)
+            dic[keys]=str(values)
         elif (keys=='uname' or keys=='posts' or keys=='groups' or keys=='last_login'):
             dic[keys]=values
     return dic
+
+print(objidconv({'_id':'o'}))

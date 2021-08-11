8128e79e0988e4455c9f8514d13343ab65f4c5ce
Imon Roy <royimonroy@gmail.com>
2021-08-11 22:07:59 +0530

    security flaw fixed

--- app/helpers.py
+++ app/helpers.py
@@ -3,10 +3,6 @@ from bson.objectid import ObjectId
 def  objidconv(inp):
     dic={}
     for keys,values in inp.items():
-        if(keys=='_id'):
-            dic[keys]=str(values)
-        elif (keys=='uname' or keys=='posts' or keys=='groups' or keys=='last_login'):
+        if (keys=='uname' or keys=='posts' or keys=='groups' or keys=='last_login'):
             dic[keys]=values
     return dic
-
-print(objidconv({'_id':'o'}))

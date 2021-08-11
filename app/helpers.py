ea96fcd963a208705c97ba2d82fa5fe3f65457fa
Imon Roy <royimonroy@gmail.com>
2021-08-11 21:56:54 +0530

    objid_fix

--- app/helpers.py
+++ app/helpers.py
@@ -1,8 +1,10 @@
-
+from bson.objectid import ObjectId
 #helper function to convert object id
 def  objidconv(inp):
     dic={}
     for keys,values in inp.items():
-        if (keys=='_id' or keys=='uname' or keys=='posts' or keys=='groups' or keys=='last_login'):
+        if(keys=='_id'):
+            dic[keys]=ObjectId(values)
+        else if (keys=='uname' or keys=='posts' or keys=='groups' or keys=='last_login'):
             dic[keys]=values
     return dic

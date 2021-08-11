f2350bac5304bdf1fa41525eac02d961261d6a67
Imon Roy <royimonroy@gmail.com>
2021-08-11 21:47:42 +0530

    bugfix

--- app/helpers.py
+++ app/helpers.py
@@ -2,7 +2,7 @@
 #helper function to convert object id
 def  objidconv(inp):
     dic={}
-    for keys,values in inp.copy():
+    for keys,values in inp.items():
         if not (keys!='_id' or keys!='uname' or keys!='posts' or keys!='groups' or keys!='last_login'):
             dic[keys]=values
     return dic

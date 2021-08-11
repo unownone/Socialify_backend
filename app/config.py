a78c5f78ea4fdb5df7e2b4fbfd510a7b7a906288
Imon Roy <royimonroy@gmail.com>
2021-08-11 18:52:12 +0530

    fixed bugs

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 class Config(object):
-    "MASTER_KEY" = os.getenv('master_key')
-    "MONGO_URI" = os.getenv('mongo_uri')
+    MASTER_KEY = os.getenv('master_key')
+    MONGO_URI = os.getenv('mongo_uri')
     
